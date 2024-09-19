df23d21441f28bb53383962d464eb08f2e8c54cb
shiv <shiv@singsys.com>
2024-09-19 14:48:54 +0530

    add cdode

--- application/controllers/Website/QrScanner.php
+++ application/controllers/Website/QrScanner.php
@@ -3,28 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class QrScanner extends CI_Controller {
     
- 
-  public function __construct() {
+    public function __construct() {
         parent::__construct();
-        $this->load->model(array('Admin_model/Adminmodel','Warehouse/Warehouse_model'));
-        $this->load->model(array('Admin_model/Role_model','Designation/Designation_model'));
-        $this->load->model('Nice_websitemodel/Nice_webmodel');
-        $this->load->model('Menu_model/Menu');
-        $this->load->helper('url');
-        $this->load->library("pagination");
-        $this->load->library('session');
-        $this->load->helper(array('url','form'));
-        $this->load->library('session');
-      
-       
-  }
+        // Load necessary models and helpers
+        $this->load->model(array(
+            'Admin_model/Adminmodel',
+            'Warehouse/Warehouse_model',
+            'Admin_model/Role_model',
+            'Designation/Designation_model',
+            'Nice_websitemodel/Nice_webmodel',
+            'Menu_model/Menu'
+        ));
+        
+        $this->load->library(array('session', 'pagination'));
+        $this->load->helper(array('url', 'form'));
+    }
 
- 	/*public function scan() {
-    // This method will be called after QR code scanning
-    $data['message'] = "Thanks for scanning";
-    $this->load->view('Ssgwebsite/website/scan/scan_result', $data);
-  }*/
- 
     public function scan() {
         $data = [];
 
@@ -52,7 +46,6 @@ class QrScanner extends CI_Controller {
         }
 
         // Load the view with data
-        $this->load->view('Ssgwebsite/website/scan', $data);
+        $this->load->view('Ssgwebsite/website/scan/scan_result', $data);
     }
 }
-
