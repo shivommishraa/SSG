f9a37a467d761456b86ac7aaaa349d2bfc52b5e4
shiv <shiv@singsys.com>
2024-10-01 16:25:13 +0530

    add

--- application/views/Ssgwebsite/website/scan/checkanswer.php
+++ application/views/Ssgwebsite/website/scan/checkanswer.php
@@ -233,7 +233,7 @@
             <!-- Buttons for Home and Quiz -->
             <div class="buttons">
                 <a href="<?php echo site_url(); ?>" class="home-button">Home</a>
-                <a href="#" class="quiz-button">Quiz</a>
+                <a href="<?php echo site_url(); ?>/Website/QrScanner/scan" class="quiz-button">Quiz</a>
             </div>
 
             <!-- Order Information -->
