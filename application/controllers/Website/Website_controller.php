931eede05d768fb0440b8b29ae608347225b83ea
shiv <shiv@singsys.com>
2024-12-23 12:25:37 +0530

    add code

--- application/controllers/Website/Website_controller.php
+++ application/controllers/Website/Website_controller.php
@@ -201,41 +201,4 @@ class Website_controller extends CI_Controller {
     $this->load->view('Ssgwebsite/website/footer');
   }
 
-
-
-   public function newindex(){ 
-    
-    $data['page_active']='index_active';
-
-    //============================ Start Pager Code ==============================
-    $galleryid=($this->input->post('galleryid')) ? $this->input->post('galleryid') :0;
-    $galleryid=($this->uri->segment(4)) ?  $this->uri->segment(4) :$galleryid;
-    
-    $this->load->config('bootstrap_pagination');
-    $config = $this->config->item('pagination_config');;
-    $config['base_url'] = base_url() ."Website/Website_controller/index"."/$galleryid";
-    $config['total_rows'] = $this->Nice_webmodel->get_count($galleryid);
-    $config['per_page'] = 20;
-    $config['uri_segment'] = 5;
-    $this->pagination->initialize($config);
-    $page = ($this->uri->segment(5)) ? $this->uri->segment(5) : 0;
-    $data['links'] = $this->pagination->create_links();
-          //============================ End Pager Code ==============================
-   
-    $data["products"] = $this->Nice_webmodel->getAllProducts($config['per_page'],$page,$galleryid);
-    if(!empty($galleryid)){ $data['searchdata']=$galleryid;}
-    $data["allbrand"] = $this->Nice_webmodel->getproduct_brand();
-    $data['product_image']=function($id){
-    return $this->Nice_webmodel->getimage_datails($id);};
-    $data['product_brand']=function($id){
-    return $this->Nice_webmodel->getproduct_brand($id);};
-           
-    //$this->load->view('Website/ms_header',$data);
-    $this->load->view('Ssgwebsite/ssg/header',$data);
-    /*$this->load->view('Ssgwebsite/ssg/bannersection');*/
-    $this->load->view('Ssgwebsite/ssg/index');
-    $this->load->view('Ssgwebsite/ssg/footer');
- 
-  }
-
 }
