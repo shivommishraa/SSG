4ce2aaa21c3da8467b9c35eadc5452112b617814
shiv <shiv@singsys.com>
2024-12-24 16:22:49 +0530

    add

--- application/models/Product_model/Tbl_product.php
+++ application/models/Product_model/Tbl_product.php
@@ -31,7 +31,7 @@ public function getSliderProduct(){
 public function getAllEnabledProductsForFeatured(){
     $this->db->select('*');
     $this->db->where('status', "1");
-    $this->db->where('enable_featured_product IS NOT NULL', "", false); // Add condition for non-NULL values
+    $this->db->where('enable_featured_product >', 0); // Check for values greater than 0
     $this->db->from('gallery');   
     $query = $this->db->get();
     return $query->result();
