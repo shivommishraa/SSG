4b355e9d86d00ce1c4c4d6664321ae58554d17ab
shiv <shiv@singsys.com>
2024-12-30 15:13:23 +0530

    add

--- application/views/Customer/Admin/editfrontendcustomer.php
+++ application/views/Customer/Admin/editfrontendcustomer.php
@@ -67,8 +67,18 @@
                                 </div>
                             </div>
                             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 ">
+                                <label for="validationCustom03">Status:</label>
+                                <select class="form-control"  name="status" id="validationCustom02"required>
+                                    <option value="0"<?php if("0"==$ctdata[0]->status) echo "selected"; ?>>No</option>
+                                    <option value="1" <?php if("0"==$ctdata[0]->status) echo "selected"; ?>>Yes</option>
+                                </select>
+                                <div class="valid-feedback">
+                                    Looks good!
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 ">
                                 <label for="validationCustom01">Profile Image:</label>
-                                <input type="file" class="form-control" required id="validationCustom01" name="customerimage" required="">
+                                <input type="file" class="form-control" required id="validationCustom01" name="customerimage">
                                 <div class="valid-feedback">
                                     Looks good!
                                 </div>
@@ -82,16 +92,6 @@
                                     <img height="150px" width="200px" src="<?php echo site_url(); ?>ssgassests/img/customerimage/<?php echo $ctdata[0]->customerimage; ?>"/>
                                 </div>
                                 <?php } ?>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 ">
-                                <label for="validationCustom03">Status:</label>
-                                <select class="form-control"  name="status" id="validationCustom02"required>
-                                    <option value="0"<?php if("0"==$ctdata[0]->status) echo "selected"; ?>>No</option>
-                                    <option value="1" <?php if("0"==$ctdata[0]->status) echo "selected"; ?>>Yes</option>
-                                </select>
-                                <div class="valid-feedback">
-                                    Looks good!
-                                </div>
-                            </div>
       </div>
       <div class="form-row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-2 pt-2">
