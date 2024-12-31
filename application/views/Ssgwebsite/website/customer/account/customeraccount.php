016ac34219cdc73e47bcb8cdc2f25bc7d3eec4d0
shiv <shiv@singsys.com>
2024-12-31 14:35:39 +0530

    add

--- application/views/Ssgwebsite/website/customer/account/customeraccount.php
+++ application/views/Ssgwebsite/website/customer/account/customeraccount.php
@@ -15,18 +15,23 @@
             </div>
         </div>
     </section>
-    <div class="list-group" id="myList" role="tablist">
-      <a class="list-group-item list-group-item-action active" data-toggle="list" href="#home" role="tab">Home</a>
-      <a class="list-group-item list-group-item-action" data-toggle="list" href="#profile" role="tab">Profile</a>
-      <a class="list-group-item list-group-item-action" data-toggle="list" href="#messages" role="tab">Messages</a>
-      <a class="list-group-item list-group-item-action" data-toggle="list" href="#settings" role="tab">Settings</a>
-    </div>
-
-    <div class="tab-content">
-      <div class="tab-pane active" id="home" role="tabpanel">...</div>
-      <div class="tab-pane" id="profile" role="tabpanel">...</div>
-      <div class="tab-pane" id="messages" role="tabpanel">...</div>
-      <div class="tab-pane" id="settings" role="tabpanel">...</div>
+    <div class="row">
+      <div class="col-4">
+        <div class="list-group" id="list-tab" role="tablist">
+          <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
+          <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+          <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
+          <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+        </div>
+      </div>
+      <div class="col-8">
+        <div class="tab-content" id="nav-tabContent">
+          <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">.1..</div>
+          <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">..2.</div>
+          <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">..3.</div>
+          <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">..4.</div>
+        </div>
+      </div>
     </div>
 
     <script>
