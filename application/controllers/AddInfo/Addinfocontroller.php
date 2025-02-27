9dcad4921bfc4d7ae90affbf83b7ac5f15a6fa21
shiv <shiv@singsys.com>
2025-02-27 12:00:20 +0530

    add

--- application/controllers/AddInfo/Addinfocontroller.php
+++ application/controllers/AddInfo/Addinfocontroller.php
@@ -216,8 +216,8 @@ class Addinfocontroller extends CI_Controller {
             $data['menu_details']=$this->Menu->getAllMenu();
             $data['admin_role']=$this->Menu->adminrole();
             $data['bannercategory']=$this->Bannercategory->getAllBannercategory();
-            $data['bannercategorybyid']=function($id){
-                return $this->Bannercategory->getBannercategoryById($id);
+            $data['bannercategorybyid']=function($bannercategory){
+                return $this->Bannercategory->getBannercategoryById($bannercategory);
             };
             $this->load->view('Dashboard/header', $data); 
             $this->load->view('Dashboard/side.php');
