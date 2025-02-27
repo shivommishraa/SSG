7eb4ace408b98222e01803fc9c65905636eee4cb
shiv <shiv@singsys.com>
2025-02-27 09:26:07 +0530

    add

--- application/views/AdditionalInfo/infobannergallery.php
+++ application/views/AdditionalInfo/infobannergallery.php
@@ -1,153 +1,108 @@
- <div class="dashboard-wrapper">
+<div class="dashboard-wrapper">
     <div class="dashboard-ecommerce">
-        <div class="container-fluid dashboard-content ">
+        <div class="container-fluid dashboard-content">
             <div class="row">
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                <div class="col-12">
                     <div class="page-header">
                         <h2 class="pageheader-title"><?php echo $title; ?></h2>
                         <div class="page-breadcrumb">
-                         <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="<?php echo site_url(); ?>Dashboard" class="breadcrumb-link">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="">Manage Additional Info</a></li>
-                                <li class="breadcrumb-item "><a href="">Info Banner Gallery</a></li>
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="<?php echo site_url(); ?>Dashboard" class="breadcrumb-link">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="#">Manage Additional Info</a></li>
+                                    <li class="breadcrumb-item active">Info Banner Gallery</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between align-items-center">
+                            <h5>Info Banner Gallery</h5>
+                            <button class="btn btn-sm btn-primary" onClick="return redirect('<?php echo site_url(); ?>AddInfo/Addinfocontroller/manageInfo');">
+                                <i class="fa fa-list" aria-hidden="true"></i> Go To Info Details List
+                            </button>
+                        </div>
+
+                        <?php if (!empty($error_msg)) { ?>
+                            <div class="col-12">
+                                <div class="alert alert-danger"><?php echo $error_msg; ?></div>
+                            </div>
+                        <?php } ?>
+
+                        <div class="card-body">
+                            <form method="post" action="" enctype="multipart/form-data">
+                                <input type="hidden" name="tbl_additional_info_id" value="1">
                                 
-                            </ol>
-                        </nav>
-                    </nav>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <label>Images:</label>
+                                        <input type="file" name="images[]" class="form-control" multiple>
+                                        <input type="hidden" name="id" value="<?php //echo !empty($gallery['id'])?$gallery['id']:''; ?>">
+                                        <input type="submit" name="imgSubmit" class="btn btn-success mt-3" value="SUBMIT">
+                                    </div>
+                                </div>
+
+                                <div class="row mt-4">
+                                    <?php if (!empty($gallery)) { ?>
+                                        <?php foreach ($gallery as $imgRow) { ?>
+                                            <div class="col-md-4" id="imgb_<?php echo $imgRow->id; ?>">
+                                                <img src="<?php echo base_url('./ssgassests/infodetailsupload/' . $imgRow->infobannerimage); ?>" class="img-fluid mt-3" alt="Info Banner">
+                                                
+                                                <div class="mt-2">
+                                                    <?php if (($infomodeldata[0]->modelpopupimage) == $imgRow->infobannerimage) { ?>
+                                                        <span class="badge badge-info">Selected As Model Popup</span>
+                                                    <?php } else { ?>
+                                                        <a href="javascript:void(0);" class="badge badge-danger" onclick="deleteImage('<?php echo $imgRow->id; ?>')">Delete</a>
+                                                        <a href="javascript:void(0);" class="badge badge-primary" onclick="setImage('<?php echo $imgRow->infobannerimage; ?>')">Set As Model Popup</a>
+                                                    <?php } ?>
+
+                                                    <br>
+
+                                                    <?php if (($infomodeldata[0]->offerimage) == $imgRow->infobannerimage) { ?>
+                                                        <span class="badge badge-warning mt-1 mb-2">Selected For Offer Page</span>
+                                                    <?php } else { ?>
+                                                        <a href="javascript:void(0);" class="badge badge-warning mt-1 mb-2" onclick="setofferImage('<?php echo $imgRow->infobannerimage; ?>')">Set For Offer Page</a>
+                                                    <?php } ?>
+
+                                                    <a href="javascript:void(0);" class="badge badge-success sendimagetowhatsapp mt-1 mb-2" onclick="sendimagetowhatsapp('<?php echo base_url('./ssgassests/infodetailsupload/' . $imgRow->infobannerimage); ?>')">Share on WhatsApp</a>
+                                                </div>
+                                            </div>
+                                        <?php } ?>
+                                    <?php } ?>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="row">
-       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-        <div class="card">
-            <h5 class="card-header">
-                <div class="row col-md-12 ">
-                 <div class="col-md-4"></div>
-                 <div class="col-md-4 "></div>
-                 <div class="col-md-4 text-right">
-                     <button class="btn btn-sm btn-primary"  onClick="return redirect('<?php echo site_url(); ?>AddInfo/Addinfocontroller/manageInfo');"><i class="fa fa-list" aria-hidden="true"></i> Go To Info Details List</button>
-                 </div>
-             </div>
-         </h5>
-         
-         <?php if(!empty($error_msg)){ ?>
-            <div class="col-xs-12">
-                <div class="alert alert-danger"><?php echo $error_msg; ?></div>
-            </div>
-        <?php } ?>
-        
-        
-        <div class="card-body">  
-            <form method="post" action="" enctype="multipart/form-data">
-               <div class="row">
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 ">
-                 
-                   <!--  <label for="validationCustom01">Name:</label> -->
-                    <input type="hidden" name="tbl_additional_info_id" value="1">
-                    <?php //echo form_error('product_name','<p class="help-block text-danger">','</p>'); ?>
-                   <!--  <div class="valid-feedback">
-                     Looks good!
-                 </div> -->
-             </div>
-             
-             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 ">
-                <label>Images:</label>
-                <input type="file" name="images[]" class="form-control" multiple>
-                <input type="hidden" name="id" value="<?php //echo !empty($gallery['id'])?$gallery['id']:''; ?>">
-
-                <input type="submit" name="imgSubmit" class="btn btn-success mt-3" value="SUBMIT">
-                <h2 class="text-center mt-3 card-header"><u></u></h2>
-                <div class="row">
-                    
-                    <?php if(!empty($gallery)){ ?>
-                     
-                        <?php foreach($gallery as $imgRow){ ?>
-                            <div class="col-md-4" id="imgb_<?php echo $imgRow->id; ?>">
-                                <img 
-                                    class="mt-5"
-                                    src="<?php echo base_url('./ssgassests/infodetailsupload/' . $imgRow->infobannerimage); ?>"  
-                                    height="80%" 
-                                    width="70%" 
-                                    alt="Info Banner"
-                                />
-                                <div style="margin-top: 10px;">
-                                    <?php if (($infomodeldata[0]->modelpopupimage) == $imgRow->infobannerimage) { ?>
-                                        <span class="badge badge-info">Selected As Model Popup</span>
-                                    <?php } else { ?>
-                                        <a 
-                                            href="javascript:void(0);" 
-                                            class="badge badge-danger" 
-                                            onclick="deleteImage('<?php echo $imgRow->id; ?>')"
-                                        >
-                                            Delete
-                                        </a>
-                                        <a 
-                                            href="javascript:void(0);" 
-                                            class="badge badge-primary" 
-                                            onclick="setImage('<?php echo $imgRow->infobannerimage; ?>')"
-                                        >
-                                            Set As Model Popup
-                                        </a>
-                                        
-                                    <?php } ?>
-                                    <br>
-                                    <?php if (($infomodeldata[0]->offerimage) == $imgRow->infobannerimage) { ?>
-                                    <span class="badge badge-warning mt-1 mb-2">Selected For Offer Page</span>
-                                    <?php } else { ?>
-
-                                        <a 
-                                            href="javascript:void(0);" 
-                                            class="badge badge-warning  mt-1 mb-2" 
-                                            onclick="setofferImage('<?php echo $imgRow->infobannerimage; ?>')">
-                                            Set For Offer Page
-                                        </a>
-                                    <?php } ?>
-                                     <a 
-                                            href="javascript:void(0);" 
-                                            class="badge badge-success sendimagetowhatsapp  mt-1 mb-2" 
-                                            onclick="sendimagetowhatsapp('<?php echo base_url('./ssgassests/infodetailsupload/' . $imgRow->infobannerimage); ?>')"
-                                        >
-                                            Share on WhatsApp
-                                        </a>
-                                </div>
-                            </div>
-
-                        <?php } ?>
-                        
-                    <?php } ?>
-                </div>
-            </div>
-        </div>
-        
-    </form>
-</div>
-</div>
-</div>
 
 <script>
-    function deleteImage(id){
-        var result = confirm("Are you sure to delete?");
-        if(result){
-            $.post( "<?php echo base_url('AddInfo/Addinfocontroller/deleteImage'); ?>", {id:id}, function(resp) {
-               if(resp != ''){
-                $('#imgb_'+id).remove();
-                alert('The image has been removed from the gallery');
-            }else{
-                alert('Some problem occurred, please try again.');
-            }
-        });
+    function deleteImage(id) {
+        if (confirm("Are you sure to delete?")) {
+            $.post("<?php echo base_url('AddInfo/Addinfocontroller/deleteImage'); ?>", { id: id }, function(resp) {
+                if (resp != '') {
+                    $('#imgb_' + id).remove();
+                    alert('The image has been removed from the gallery');
+                } else {
+                    alert('Some problem occurred, please try again.');
+                }
+            });
         }
     }
 
-   function setImage(image) {
-        var result = confirm("Are you sure to set this as an info banner?");
-        if (result) {
+    function setImage(image) {
+        if (confirm("Are you sure to set this as an info banner?")) {
             $.post("<?php echo base_url('AddInfo/Addinfocontroller/setImageAsModelPopup'); ?>", { image: image }, function(resp) {
                 if (resp != '') {
                     alert('The image has been set as an Info Banner.');
-                    location.reload(); // Reload the page after success
+                    location.reload();
                 } else {
                     alert('Some problem occurred, please try again.');
                 }
@@ -155,13 +110,12 @@
         }
     }
 
-     function setofferImage(image) {
-        var result = confirm("Are you sure to set this as an info banner?");
-        if (result) {
+    function setofferImage(image) {
+        if (confirm("Are you sure to set this as an offer banner?")) {
             $.post("<?php echo base_url('AddInfo/Addinfocontroller/setImageForOffer'); ?>", { image: image }, function(resp) {
                 if (resp != '') {
-                    alert('The image has been set as an Info Banner.');
-                    location.reload(); // Reload the page after success
+                    alert('The image has been set as an Offer Banner.');
+                    location.reload();
                 } else {
                     alert('Some problem occurred, please try again.');
                 }
@@ -169,12 +123,9 @@
         }
     }
 
-
     function sendimagetowhatsapp(imageUrl) {
-        // Open WhatsApp share window with the image URL
-        const whatsappUrl = "https://wa.me/?text=" + encodeURIComponent("Check out this image: " + imageUrl);
-        window.open(whatsappUrl, '_blank');
+        window.open("https://wa.me/?text=" + encodeURIComponent("Check out this image: " + imageUrl), '_blank');
     }
-
 </script>
+
 <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
